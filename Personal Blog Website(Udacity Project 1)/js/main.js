64b49141541313ac67d79dfe590e553a8175f5a5
Vikrant Malla <48061750+vikrantmalla@users.noreply.github.com>
2020-05-27 00:41:19 -0700

    Update main.js

--- Personal Blog Website(Udacity Project 1)/js/main.js
+++ Personal Blog Website(Udacity Project 1)/js/main.js
@@ -23,26 +23,4 @@ $(document).ready(function () {
         $nav.toggleClass('collapse');
     })
 
-    // owl-crousel for blog
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: false,
-        autoplayTimeout: 3000,
-        dots: false,
-        nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-        responsive: responsive
-    });
-
-
-    // click to scroll top
-    $('.move-up span').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1000);
-    })
-
-    // AOS Instance
-    AOS.init();
-
 });
